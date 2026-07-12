a6b5cf87ed42ef0bd1ddbb64a41d01d5c22e759f
ptakondrej <florixakcz@seznam.cz>
2026-07-12 20:17:00 +0200

    chore: import feature modules into AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { CatalogModule } from './catalog/catalog.module';
+import { AuthModule } from './auth/auth.module';
+import { MatchingModule } from './matching/matching.module';
+import { OwnedPartsModule } from './owned-parts/owned-parts.module';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule],
+  imports: [
+    AppConfigModule,
+    DatabaseModule,
+    CatalogModule,
+    OwnedPartsModule,
+    MatchingModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
