5bdbbee0ad8d52f18a8695edab115d19c3f23d32
Ondřej Pták <75623781+florixak@users.noreply.github.com>
2026-07-14 18:19:28 +0200

    feat: implement rate limiting (#20)
    * feat: create rate limit values config
    
    * feat: create throttle decorator
    
    * chore: setup throttle in app module
    
    * feat: add auth throttle to auth endpoints

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,17 +7,25 @@ import { CatalogModule } from './catalog/catalog.module';
 import { AuthModule } from './auth/auth.module';
 import { MatchingModule } from './matching/matching.module';
 import { OwnedPartsModule } from './owned-parts/owned-parts.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { RATE_LIMITS } from './config/rate-limit.config';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     AppConfigModule,
     DatabaseModule,
+    ThrottlerModule.forRoot([
+      { name: 'default', ...RATE_LIMITS.default },
+      { name: 'auth', ...RATE_LIMITS.auth },
+      { name: 'matching', ...RATE_LIMITS.matching },
+    ]),
     CatalogModule,
     OwnedPartsModule,
     MatchingModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
