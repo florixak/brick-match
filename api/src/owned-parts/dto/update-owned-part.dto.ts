156ce10b448160274202447da69300dd376a9940
ptakondrej <florixakcz@seznam.cz>
2026-07-15 11:18:46 +0200

    refactor: remove redundant dtos

--- api/src/owned-parts/dto/update-owned-part.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateOwnedPartDto } from './create-owned-part.dto';
-
-export class UpdateOwnedPartDto extends PartialType(CreateOwnedPartDto) {}
