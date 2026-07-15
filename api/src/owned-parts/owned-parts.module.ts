cc739158c969debc147ed4e04567a6a6e8346bfa
ptakondrej <florixakcz@seznam.cz>
2026-07-15 14:14:06 +0200

    chore: import database module

--- api/src/owned-parts/owned-parts.module.ts
+++ api/src/owned-parts/owned-parts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OwnedPartsService } from './owned-parts.service';
 import { OwnedPartsController } from './owned-parts.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [OwnedPartsController],
   providers: [OwnedPartsService],
 })
