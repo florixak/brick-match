c89cf7fd09c2d6b18cba5b7d78408b0d72836aae
ptakondrej <florixakcz@seznam.cz>
2026-07-18 16:26:24 +0200

    fix: exported hook name

--- web/lib/queries/catalog/themes.ts
+++ web/lib/queries/catalog/themes.ts
@@ -22,6 +22,6 @@ export function catalogThemesQueryOptions() {
   })
 }
 
-export function useCatalogColors() {
+export function useCatalogThemes() {
   return useQuery(catalogThemesQueryOptions())
 }
