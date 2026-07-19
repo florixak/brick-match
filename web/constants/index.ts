2a776708fe6b4b929c6870104cc9a53ad6ed7c24
ptakondrej <florixakcz@seznam.cz>
2026-07-19 07:56:46 +0200

    chore: create constants file

---
+++ web/constants/index.ts
@@ -0,0 +1,50 @@
+import {
+  BoxIcon,
+  HomeIcon,
+  type LucideIcon,
+  PuzzleIcon,
+  SearchIcon,
+  UserIcon,
+} from "lucide-react"
+import type { UrlObject } from "url"
+
+type NavLink = {
+  label: string
+  href: UrlObject
+  icon: LucideIcon
+}
+
+export const NAV_LINKS: NavLink[] = [
+  {
+    label: "Search",
+    href: {
+      pathname: "/",
+    },
+    icon: SearchIcon,
+  },
+  {
+    label: "My Parts",
+    href: {
+      pathname: "/owned-parts",
+    },
+    icon: BoxIcon,
+  },
+  {
+    label: "Matching",
+    href: {
+      pathname: "/matching",
+    },
+    icon: PuzzleIcon,
+  },
+]
+
+export const MOBILE_NAV_LINKS: NavLink[] = [
+  ...NAV_LINKS,
+  {
+    label: "Me",
+    href: {
+      pathname: "/me",
+    },
+    icon: UserIcon,
+  },
+]
