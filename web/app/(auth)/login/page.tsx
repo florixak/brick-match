514cdfc99d12a060db4102907a813d86048db99f
ptakondrej <florixakcz@seznam.cz>
2026-07-21 07:21:52 +0200

    feat: render login components

--- web/app/(auth)/login/page.tsx
+++ web/app/(auth)/login/page.tsx
@@ -1,5 +1,22 @@
+import AuthCard from "@/components/auth/auth-card"
+import LoginForm from "@/components/auth/login-form"
+import Studs from "@/components/layout/studs"
+
 const LoginPage = () => {
-  return <div>LoginPage</div>
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-full max-w-sm">
+        <Studs count={5} />
+        <AuthCard
+          title="Welcome Back"
+          subtitle="Log in to BrickMatch"
+          type="login"
+        >
+          <LoginForm />
+        </AuthCard>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
