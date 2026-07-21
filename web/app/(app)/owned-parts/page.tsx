72234a0e9868bd6100e58804cb81933fb593a676
ptakondrej <florixakcz@seznam.cz>
2026-07-21 12:25:59 +0200

    feat: render filters and hero component

--- web/app/(app)/owned-parts/page.tsx
+++ web/app/(app)/owned-parts/page.tsx
@@ -1,5 +1,13 @@
+import Filters from "@/components/owned-parts/filters"
+import Hero from "@/components/owned-parts/hero"
+
 const OwnedPartsPage = () => {
-  return <div>OwnedPartsPage</div>
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <Hero />
+      <Filters />
+    </div>
+  )
 }
 
 export default OwnedPartsPage
